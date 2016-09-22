fa57dacdad4b0e186985569824480f3c95dfdafd
Randy D. Binondo <randy@crowdmetric.com>
2016-09-22 04:10:50 +0000

    update

--- resources/views/main/checkout.blade.php
+++ resources/views/main/checkout.blade.php
@@ -1,61 +1,176 @@
 @extends('layouts.default')
 
+@section('styles')
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('/css/styles.css') }}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('/css/jquery.fancybox.css') }}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('/css/style.css') }}">
+@endsection
+
 @section('content')
-    <main id="content">
-        <section class="row-1">
-            <div class="inner clear">
-                <form>
-                    <h2>
-                        <span class="bold">FINAL STEP</span> - Activate Your Account Below.
-                    </h2>
+    <div class="outer-wrapper">
+        <div class="content-sub-wrapper">
+            <div class="container">
+                <div class="sub-content">
+                    <div class="heading-panel">
+                        <div class="text-center">
+                            <h2 class="sub-heading-title">
+                                <span>FINAL STEP</span> - Activate Your Account Below.
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="content-wraper">
+                        <div class="content-inner-wrap" id="checkout">
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6">
 
-                    <div class="col left">
-                        <div class="fields">
-                            <div class="field input">
-                                <span>1. Amount</span>
-                                <label>Amount (USD $)</label>
-                                <input type="text"/>
-                            </div>
-                        </div>
-                        <div class="fields">
-                            <span>2. Billing Information</span>
-                            <div class="field input">
-                                <label>Amount (USD $)</label>
-                                <input type="text"/>
-                            </div>
-                        </div>
-                        <div class="fields">
-                            <span>3. Payment Information</span>
-                            <div class="field input">
-                                <input type="text" name="cardnumber"/>
-                            </div>
-                            <div class="field input">
-                                <input type="text" name="cardnumber"/>
+                                </div>
+                                <div class="col-xs-12 col-md-6">
+                                    <div class="box-form-wrap clearfix" id="checkout-form">
+                                        <form>
+                                            <div class="box-form-item" id="payments-info">
+                                                <div class="box-heading clearfix">
+                                                    <h2>1. Amount</h2>
+                                                </div>
+
+                                                <div class="box-form-inputs padding-none clearfix">
+                                                    <label class="field">
+															<span class="col-xs-12 col-sm-8">
+																<span class="col-radio">
+																	<input type="radio" name="package" value="1" id="recommended">
+																</span>
+																<span class="col-label">
+																	<strong>RECOMMENDED</strong>
+																	<small>1 month of UNLIMITED Docs</small>
+																</span>
+															</span>
+															<span class="col-xs-12 col-sm-4 price">
+																<strong>$22.95/mo</strong>
+																<small class="popular">Most Popular</small>
+															</span>
+                                                    </label>
+                                                    <label class="field">
+															<span class="col-xs-12 col-sm-8">
+																<span class="col-radio">
+																	<input type="radio" name="package" value="1" id="recommended">
+																</span>
+																<span class="col-label">
+																	<strong>MODERATE USERS</strong>
+																	<small>3 months of UNLIMITED Docs</small>
+																</span>
+															</span>
+															<span class="col-xs-12 col-sm-4 price">
+																<strong>$14.95/mo</strong>
+																<small>($44.85 today)</small>
+															</span>
+                                                    </label>
+                                                </div>
+
+                                                <div class="box-heading clearfix">
+                                                    <h2>2. Billing Information</h2>
+                                                </div>
+
+                                                <div class="box-form-inputs clearfix">
+                                                    <div class="input">
+                                                        <label>First Name</label>
+                                                        <div class="form-group box-input-name">
+                                                            <input type="text" placeholder="First Name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="input">
+                                                        <label>Last Name</label>
+                                                        <div class="form-group box-input-name">
+                                                            <input type="text" placeholder="Last Name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="input">
+                                                        <label>Email Address</label>
+                                                        <div class="form-group box-input-name">
+                                                            <input type="text" placeholder="Email Address">
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+
+                                                <div class="box-heading clearfix">
+                                                    <h2>3. Payment Information</h2>
+                                                </div>
+
+                                                <div class="box-form-inputs clearfix">
+                                                    <div class="form-group box-input-name">
+                                                        <input type="text" placeholder="Card Number">
+                                                    </div>
+                                                    <div class="form-group box-input-name">
+                                                        <input type="text" placeholder="Cardholder Name">
+                                                    </div>
+                                                    <div class="form-group-expiration clearfix">
+															<span class="exp-label-wrap">
+																<label>Expiration Date</label>
+
+																<span class="step-card"><img src="images/card-5.png"></span>
+															</span>
+                                                        <div class="expiration-form-inputs clearfix">
+																<span class="exp-date col-xs-12 col-sm-4">
+																	<select>
+                                                                        <option>09(Sep)</option>
+                                                                        <option>10(Sep)</option>
+                                                                        <option>11(Sep)</option>
+                                                                        <option>12(Sep)</option>
+                                                                        <option>13(Sep)</option>
+                                                                        <option>14(Sep)</option>
+                                                                        <option>15(Sep)</option>
+                                                                    </select>
+																</span>
+																<span class="exp-yr col-xs-12 col-sm-4">
+																	<select>
+                                                                        <option>2016</option>
+                                                                        <option>2015</option>
+                                                                        <option>2014</option>
+                                                                        <option>2013</option>
+                                                                        <option>2012</option>
+                                                                        <option>2011</option>
+                                                                        <option>2010</option>
+                                                                    </select>
+																</span>
+																<span class="exp-cvv col-xs-12 col-sm-4">
+																	<input type="text" placeholder="CVV">
+																</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="secured clearfix">
+                                                    <input type="checkbox" />
+														<span>
+														By clicking “Continue,” I am providing my electronic signature authorizing Postal Exam to charge my credit card as described, until I cancel. I understand that I will be charged $22.86 every month until I cancel my account, which can be done at any time, hassle free by contacting FormsLibrary’s US-based member care center.
+														</span>
+                                                </div>
+                                                <div class="footer-form-group clearfix">
+                                                    <input type="button" value="Click here to pay now">
+                                                </div>
+                                            </div><!-- end of box-form-item -->
+                                        </form>
+                                    </div>
+                                    <div class="box-guarantee-wrap">
+                                        <img src="images/guarantee.png">
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-
-                    <div class="col right">
-                        <h3>Terms of Purchase</h3>
-                        <p>
-                            <ol>
-                                <li>Your purchase will appear on your bank statement under the name "POSTALEXAM.US</li>
-
-                                <li>Your current IP Address and other information has been recorded. All attempts at fraud will be prosecuted.</li>
-
-                                <li>Postal Exam is provided by United States Postal Service.</li>
-
-                                <li>Postal Exam reserves all rights.</li>
-
-                                <li>All information submitted here is subjected to <a href="https://www.postalexam.us.com">Postal Exam</a> privacy policy</li>
-                            </ol>
-                        </p>
-                        <div>
-                            <div class="note"><span class="icon">To avoid being charged twice, only click the button once.</span></div>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
-        </section>
-    </main>
+        </div><!-- end of content-sub-wrapper -->
+        <div class="footer-sub-wrap"></div><!-- end of footer -->
+    </div>
+@endsection
+
+
+@section('footer')
+    @parent
+
+    <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+
+
+
 @endsection
