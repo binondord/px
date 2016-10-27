b44f24aa302a6af8ef916a11b44717f2b78d1293
Randy D. Binondo <randy@crowdmetric.com>
2016-10-27 02:50:15 +0000

    update

--- resources/views/sites/postalexam/5/checkout.blade.php
+++ resources/views/sites/postalexam/5/checkout.blade.php
@@ -69,10 +69,43 @@
                                     <div class="col-xs-12 col-md-7">
                                         <div class="row">
                                             <div class="col-xs-6">
-                                                <input id="exp-date-1" type="number"/>
+                                                <select name="month" required="">
+                                                    <option value="">Month</option>
+                                                    <option>01</option>
+                                                    <option>02</option>
+                                                    <option>03</option>
+                                                    <option>04</option>
+                                                    <option>05</option>
+                                                    <option>06</option>
+                                                    <option>07</option>
+                                                    <option>08</option>
+                                                    <option>09</option>
+                                                    <option>10</option>
+                                                    <option>11</option>
+                                                    <option>12</option>
+                                                </select>
                                             </div>
-                                            <div class="col-xs-6">
-                                                <input type="number"/>
+                                            <div class="col-xs-1 separator"><span> / </span></div>
+                                            <div class="col-xs-5">
+                                                <select name="year" required="">
+                                                    <option value="">Year</option>
+                                                    <option>2016</option>
+                                                    <option>2017</option>
+                                                    <option>2018</option>
+                                                    <option>2019</option>
+                                                    <option>2020</option>
+                                                    <option>2021</option>
+                                                    <option>2022</option>
+                                                    <option>2023</option>
+                                                    <option>2024</option>
+                                                    <option>2025</option>
+                                                    <option>2026</option>
+                                                    <option>2027</option>
+                                                    <option>2028</option>
+                                                    <option>2029</option>
+                                                    <option>2030</option>
+                                                    <option>2031</option>
+                                                </select>
                                             </div>
                                         </div>
                                     </div>
